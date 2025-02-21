bd359408ca04892443aed0057790ca41140167d3
Rishi <rishiawasthi6261@gmail.com>
2025-02-21 20:14:43 +0530

    Device add function to living room

--- src/com/javacasestudy2/House/Livingroom.java
+++ src/com/javacasestudy2/House/Livingroom.java
@@ -1,8 +1,11 @@
 package com.javacasestudy2.House;
 
 import com.javacasestudy2.CategoryOfDevices.LivingroomDevices;
+import com.javacasestudy2.Device.*;
 
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Livingroom extends Rooms{
 
@@ -18,7 +21,43 @@ public class Livingroom extends Rooms{
     }
 
     public boolean addDevice(){
-        return true;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Devise you would like to install");
+        System.out.println("+----------------------------------------------+");
+        System.out.println("1. AirConditioner   press-1");
+        System.out.println("2. Fan              press-2");
+        System.out.println("3. Light            press-3");
+        System.out.println("4. Television       press-4");
+        System.out.println("5. Geyser           press-5");
+        System.out.println("6. ExhaustFan       press-6");
+        System.out.println("+----------------------------------------------+");
+        System.out.println("Enter your preferred choice");
+        int choice = sc.nextInt();
+        Random rand = new Random();
+
+        switch (choice){
+            case 1:
+                livingRoomDevices.add(new AirConditioner(rand.nextInt(1000),"AirConditioner",false));
+                return false;
+            case 2:
+                livingRoomDevices.add(new Fan(rand.nextInt(1000),"Fan",false));
+
+                return true;
+            case 3:
+                livingRoomDevices.add(new Light(rand.nextInt(1000),"Light",false));
+                return true;
+            case 4:
+                livingRoomDevices.add(new Television(rand.nextInt(1000),"Television",false));
+                return false;
+            case 5:
+                System.out.println("You cant add Geyser to the LivingRoom");
+                return false;
+            case 6:
+                System.out.println("You cant add Exhaust to the LivingRoom");
+                return false;
+            default:
+                return false;
+        }
     }
 
     public void display() {}
